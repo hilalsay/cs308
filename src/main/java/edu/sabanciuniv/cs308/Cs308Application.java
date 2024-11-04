a4d0d6a8c4f5dbdc8d4d53ff51bb534a18d728e1
hilalsay <hilalsaydogdu09@gmail.com>
2024-11-04 21:51:06 +0300

    Initial commit

---
+++ src/main/java/edu/sabanciuniv/cs308/Cs308Application.java
@@ -0,0 +1,13 @@
+package edu.sabanciuniv.cs308;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs308Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs308Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/sabanciuniv/cs308/Cs308ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sabanciuniv.cs308;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs308ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
