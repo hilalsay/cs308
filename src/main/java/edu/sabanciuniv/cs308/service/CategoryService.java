d2470989b26db2086c5ebfa60daf16a4de93b698
Elif Sude Meydan <elifsude@sabanciuniv.edu>
2024-11-08 16:44:35 +0300

    service annotation added

--- src/main/java/edu/sabanciuniv/cs308/service/CategoryService.java
+++ src/main/java/edu/sabanciuniv/cs308/service/CategoryService.java
@@ -3,10 +3,12 @@ package edu.sabanciuniv.cs308.service;
 import edu.sabanciuniv.cs308.model.Product;
 import edu.sabanciuniv.cs308.repo.CategoryRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
 
+@Service
 public class CategoryService {
     @Autowired
     private CategoryRepo repo;
